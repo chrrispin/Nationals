384283b9fca120bc67e306caccdba8f039f4f4b3
chrrispin <chrrispin@gmail.com>
2025-11-08 14:11:59 +0200

    now

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="h-screen w-full bg-white flex items-center justify-center">
+      <div className="text-2xl font-bold">
+        Hello world
+      </div>
+    </div>
+  )
+}
+
+export default App
